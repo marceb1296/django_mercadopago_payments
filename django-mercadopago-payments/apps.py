fab57005b24151da7c8c20ebd8eb639054fb0eae
marceh96 <marce_1996hr@hotmail.com>
2021-11-26 22:26:06 -0600

    first commit

---
+++ django-mercadopago-payments/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class DjangoMercadopagoPaymentsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'django_mercadopago_payments'
